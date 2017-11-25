5b4896eaa89d301c69f87324240dec84ed2e5bab
Arthur Dassier <arthur.dassier@epitech.eu>
2017-11-25 17:48:42 +0100

    Norme

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -10,13 +10,13 @@
 
 void main(int argc, char *argv[])
 {
-	if (argc > 600)
-		return;
 	Chainlist	*list_a = init(my_getnbr(argv[1]));
 	Chainlist	*list_b = NULL;
 	char 	*str = malloc(sizeof(char) * 400000);
 
-	for (int i = 2;i != argc;i++)
+	if (argc > 600)
+		return;
+	for (int i = 2 ; i != argc ; i++)
 		insert_end(&list_a, my_getnbr(argv[i]));
 	while (is_sup(list_a) == 1) {
 		if (is_sup(list_a) == 1)
